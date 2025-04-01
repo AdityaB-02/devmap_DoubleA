174c3cb6883a7ad9ecffb32c3026962c160d9736
AB02 <abp.2.mace@gmail.com>
2025-04-01 19:31:34 +0530

    Task 3

---
+++ TASK_4/script.js
@@ -0,0 +1,18 @@
+function calculate() {
+    try {
+        let expression = document.getElementById("expression").value;
+        if (!/[\d+\-*/().]/.test(expression)) {
+            throw new Error("Invalid characters detected.");
+        }
+        let result = eval(expression);
+        document.getElementById("result").innerText = "Result: " + result;
+    } catch (error) {
+        document.getElementById("result").innerText = "Error: Invalid Expression";
+    }
+}
+
+document.getElementById('expression').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      calculate();
+    }
+  });
